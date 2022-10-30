a3c559efb079772efb7de9fecef411480e481404
vishalkumargourav <vishalkumargourav@github.com>
2022-10-30 21:06:30 +0530

    @Configuration and @Bean in Java based configuration

--- src/main/java/vkg/springframework/vkgdi/config/GreetingServiceConfig.java
+++ src/main/java/vkg/springframework/vkgdi/config/GreetingServiceConfig.java
@@ -3,6 +3,8 @@ package vkg.springframework.vkgdi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import vkg.springframework.vkgdi.services.ConstructorInjectedGreetingService;
+import vkg.springframework.vkgdi.services.PropertyInjectedGreetingService;
+import vkg.springframework.vkgdi.services.SetterInjectedGreetingService;
 
 @Configuration
 public class GreetingServiceConfig {
@@ -10,5 +12,15 @@ public class GreetingServiceConfig {
     @Bean
     ConstructorInjectedGreetingService constructorInjectedGreetingService(){
         return new ConstructorInjectedGreetingService();
+    }
+
+    @Bean
+    PropertyInjectedGreetingService propertyInjectedGreetingService(){
+        return new PropertyInjectedGreetingService();
+    }
+
+    @Bean
+    SetterInjectedGreetingService setterInjectedGreetingService(){
+        return new SetterInjectedGreetingService();
     }
 }
